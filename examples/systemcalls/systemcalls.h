ecfc7bbf1ed19a0f3df78054811d3088cd042ead
swathist <swathi.st2708@gmail.com>
2025-03-29 17:53:07 +0530

    Assignment 3-1

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
 bool do_system(const char *command);
 
