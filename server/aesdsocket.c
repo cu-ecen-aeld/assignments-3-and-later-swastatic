621a78d5f3120476e4c3c1a1024346dd3bcf4b68
swathist <swathi.st2708@gmail.com>
2025-04-22 21:55:06 +0530

    Assignment 6-1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -32,72 +32,101 @@ static void signal_handler ( int signal_number )
 
 struct thread_data{
 	int sockfd;
-	//char *buff;
-	char buff[32];
-	bool ret_stat;
+	char buff[1024];
+	char client_ip[16];
+	int new_fd;
+	pthread_t thread;
 };
 
+struct node{
+	struct thread_data data;
+	struct node *next;
+};
+
+struct node * create_node()
+{
+	struct node * new_node = (struct node *)malloc(sizeof(struct node));
+	new_node->next = NULL;
+	return new_node;
+}
+
+pthread_mutex_t mutex;
 void* threadfunc(void* thread_param)
 {
 	struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-	//thread_func_args->buff = (char *)malloc(64);
-	struct sockaddr_in their_addr;
+
+	pthread_mutex_lock(&mutex);
+	//Open the file and write buff value into it
+	int buff_fd = open("/var/tmp/aesdsocketdata", O_RDWR|O_CREAT|O_APPEND, S_IRWXU|S_IRWXG|S_IRWXO);
+
+	int rd = recv(thread_func_args->new_fd, thread_func_args->buff, sizeof(thread_func_args->buff)-1, 0);
+	thread_func_args->buff[rd] = '\0';
+	while(rd > 0)
+	{
+		//Receiving data from the socket
+		char *newline = strchr(thread_func_args->buff, '\n');
+		if (newline) {
+			size_t index = newline - thread_func_args->buff;
+			thread_func_args->buff[index+1] = '\0';
+		}
+		unsigned char len = strlen(thread_func_args->buff);
+		write(buff_fd, thread_func_args->buff, len);
+		if (newline)
+			break;
+		memset(thread_func_args->buff, 0, sizeof(thread_func_args->buff));
+		rd = recv(thread_func_args->new_fd, thread_func_args->buff, sizeof(thread_func_args->buff)-1, 0);
+		thread_func_args->buff[rd] = '\0';
+	}
+
+	lseek(buff_fd, 0, SEEK_SET);
+	ssize_t sent = read(buff_fd, thread_func_args->buff, sizeof(thread_func_args->buff)-1);
+	thread_func_args->buff[sent] = '\0';
+	while(sent > 0)
+	{
+		send(thread_func_args->new_fd, thread_func_args->buff, strlen(thread_func_args->buff), 0);
+		memset(thread_func_args->buff, 0, sizeof(thread_func_args->buff));
+		sent = read(buff_fd, thread_func_args->buff, sizeof(thread_func_args->buff)-1);
+		thread_func_args->buff[sent] = '\0';
+	}
+	pthread_mutex_unlock(&mutex);
+	syslog(LOG_INFO, "Closed connection from %s\n", thread_func_args->client_ip);
+	memset(thread_func_args->buff, 0, sizeof(thread_func_args->buff));
+	close(buff_fd);
+	close(thread_func_args->new_fd);
+	pthread_exit(&thread_func_args->thread);
+	return NULL;
+}
+
+void* time_writer(void* thread_param)
+{
+	char timestr[200];
+	time_t t;
+	struct tm *tmp;
 
 	while(1)
 	{
-		socklen_t addrlen = sizeof(struct sockaddr_in);
-		//Accept connections
-		int new_fd = accept(thread_func_args->sockfd, (struct sockaddr *)&their_addr, &addrlen);
-		if (new_fd == -1) {
-			perror("accept");
-			close(thread_func_args->sockfd);
-			thread_func_args->ret_stat = false;
-			exit(1);
-		}
-		
-		char client_ip[16] = "";
-		inet_ntop(AF_INET, &their_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
-		//printf("Accepted connection from %s\n", client_ip);
-		syslog(LOG_INFO, "Accepted connection from %s\n", client_ip);
-
-		//Open the file and write buff value into it
-		int buff_fd = open("/var/tmp/aesdsocketdata", O_RDWR|O_CREAT|O_APPEND, S_IRWXU|S_IRWXG|S_IRWXO);
-
-		int rd = recv(new_fd, thread_func_args->buff, sizeof(thread_func_args->buff)-1, 0);
-		thread_func_args->buff[rd] = '\0';
-		while(rd > 0)
-		{
-			//Receiving data from the socket
-			char *newline = strchr(thread_func_args->buff, '\n');
-			if (newline) {
-				size_t index = newline - thread_func_args->buff;
-				thread_func_args->buff[index+1] = '\0';
-			}
-			unsigned char len = strlen(thread_func_args->buff);
-			write(buff_fd, thread_func_args->buff, len);
-			if (newline)
-				break;
-			memset(thread_func_args->buff, 0, sizeof(thread_func_args->buff));
-			rd = recv(new_fd, thread_func_args->buff, sizeof(thread_func_args->buff)-1, 0);
-			thread_func_args->buff[rd] = '\0';
+		sleep(10);
+		t = time(NULL);
+		tmp = localtime(&t);
+		if (tmp == NULL) {
+			perror("localtime");
+			exit(EXIT_FAILURE);
 		}
 
-		lseek(buff_fd, 0, SEEK_SET);
-		ssize_t sent = read(buff_fd, thread_func_args->buff, sizeof(thread_func_args->buff)-1);
-		thread_func_args->buff[sent] = '\0';
-		while(sent > 0)
-		{
-			send(new_fd, thread_func_args->buff, strlen(thread_func_args->buff), 0);
-			memset(thread_func_args->buff, 0, sizeof(thread_func_args->buff));
-			sent = read(buff_fd, thread_func_args->buff, sizeof(thread_func_args->buff));
-			thread_func_args->buff[sent] = '\0';
+		if (strftime(timestr, sizeof(timestr), "%Y-%m-%d %H:%M:%S", tmp) == 0) {
+			fprintf(stderr, "strftime returned 0");
+			exit(EXIT_FAILURE);
 		}
-		//printf("Closed connection from %s\n", client_ip);
-		syslog(LOG_INFO, "Closed connection from %s\n", client_ip);
-		memset(thread_func_args->buff, 0, sizeof(thread_func_args->buff));
-		close(buff_fd);
-		close(new_fd);
+
+		pthread_mutex_lock(&mutex);
+		int time_fd = open("/var/tmp/aesdsocketdata", O_RDWR|O_CREAT|O_APPEND, S_IRWXU|S_IRWXG|S_IRWXO);
+		write(time_fd, "timestamp:", 10);
+		write(time_fd, timestr, strlen(timestr));
+		write(time_fd, "\n", 1);
+		close(time_fd);
+		pthread_mutex_unlock(&mutex);
 	}
+	return NULL;
 }
 
 int main(int argc, char **argv)
@@ -139,7 +168,7 @@ int main(int argc, char **argv)
 		return -1;
 	}
 	freeaddrinfo(result);
-	
+
 	//Creating a fork process after successful bind
 	pid_t chipid = fork();
 	if(chipid == 0)
@@ -152,48 +181,98 @@ int main(int argc, char **argv)
 		}
 		printf("Listening for connections...\n");
 
-		//Setting up the pthread for the while loop to read and send the content
-		pthread_t thread;
-		int rc = pthread_create(&thread, NULL, threadfunc, &params);
+		if (pthread_mutex_init(&mutex, NULL) != 0) {
+        		perror("mutex initialization failed");
+        		exit(EXIT_FAILURE);
+		}
+
+		pthread_t timethread;
+		struct node * head = NULL;
+		struct node * last = NULL;
+
+		int rc = pthread_create(&timethread, NULL, time_writer, NULL);
 		if (rc != 0)
 		{
 			printf("Creation of pthread failed!\n");
 			return false;
 		}
-		printf("Successfully created pthread!\n");
 
 		//Setting up the signals
 		struct sigaction new_action;
-		bool success = true;
 		memset(&new_action,0,sizeof(struct sigaction));
 		new_action.sa_handler=signal_handler;
 		if( sigaction(SIGTERM, &new_action, NULL) != 0 ) {
 			printf("Error %d (%s) registering for SIGTERM",errno,strerror(errno));
-			success = false;
 		}
 		if( sigaction(SIGINT, &new_action, NULL) ) {
 			printf("Error %d (%s) registering for SIGINT",errno,strerror(errno));
-			success = false;
 		}
 
-		if( success ) {
-			printf("Waiting forever for a signal\n");
-			pause();
-			if( caught_sigint )
-				printf("\nCaught SIGINT!\n");
-			if( caught_sigterm )
-				printf("\nCaught SIGTERM!\n");
+		while(!caught_sigint && !caught_sigterm)
+		{
+			socklen_t addrlen = sizeof(struct sockaddr_in);
+			//Accept connections
+			struct sockaddr_in their_addr;
+			params.new_fd = accept(params.sockfd, (struct sockaddr *)&their_addr, &addrlen);
+			if (params.new_fd == -1) {
+				perror("accept");
+				if (errno == EINTR) {
+					break;
+				}
+				continue;
+			}
+			inet_ntop(AF_INET, &their_addr.sin_addr, params.client_ip, INET_ADDRSTRLEN);
+			syslog(LOG_INFO, "Accepted connection from %s\n", params.client_ip);
 
-			syslog(LOG_INFO, "Caught signal, exiting\n");
-			close(params.sockfd);
-			system("rm -rf /var/tmp/aesdsocketdata");
+			struct node *thread_node = create_node();
+			thread_node->data.sockfd = params.sockfd;
+        		strcpy(thread_node->data.client_ip, params.client_ip);
+			thread_node->data.new_fd = params.new_fd;
+			if(last == NULL)
+			{
+				head = thread_node;
+				last = head;
+			}
+			else
+			{
+				last->next = thread_node;
+				last = thread_node;
+			}
+
+			//Setting up the pthread for the while loop to read and send the content
+			rc = pthread_create(&(thread_node->data.thread), NULL, threadfunc, &(thread_node->data));
+			if (rc != 0)
+			{
+				printf("Creation of pthread failed!\n");
+				struct node *temp = head;
+				while (temp != NULL) {
+					pthread_join(temp->data.thread, NULL);
+					struct node *to_free = temp;
+					temp = temp->next;
+					free(to_free);
+				}
+				return false;
+			}
+		}
+
+		if( caught_sigint )
+			printf("\nCaught SIGINT!\n");
+		if( caught_sigterm )
+			printf("\nCaught SIGTERM!\n");
+
+		syslog(LOG_INFO, "Caught signal, exiting\n");
+		close(params.sockfd);
+		pthread_mutex_destroy(&mutex);
+		system("rm -rf /var/tmp/aesdsocketdata");
+		//pthread_join(timethread, NULL);
+		struct node *temp = head;
+		while (temp != NULL) {
+			pthread_join(temp->data.thread, NULL);
+			struct node *to_free = temp;
+			temp = temp->next;
+			free(to_free);
 		}
 	}
-	/*else
-	{
-		if(waitpid(chipid, &status, 0) == -1)
-			return -1;
-		else if(WIFEXITED(status))
-			return 0;
-	}*/
+	else
+		exit(EXIT_SUCCESS);
 }
